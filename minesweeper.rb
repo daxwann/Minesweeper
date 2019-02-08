bfbbda001507eb673c59f5d1e64a55f4a397d43d
Xiaoda Wang <sig.xiaoda@gmail.com>
2019-02-07 19:03:52 -0800

    add UI

---
+++ minesweeper.rb
@@ -0,0 +1,63 @@
+require_relative "board"
+require_relative "player"
+require "colorize"
+
+class Minesweeper
+  def initialize()
+    @board = Board.new(9, 10)
+    @player = Player.new(9)    
+  end
+
+  def start
+    until @board.revealed_all_safe?
+      system("clear")
+      @board.render
+      coord = @player.get_coordinate
+      command = @player.get_command
+      if !self.safe_pick?(coord, command)
+        self.game_over()
+        return
+      end
+    end
+    self.you_win()    
+  end
+
+  def safe_pick?(coord, command)
+    if command == 'r'
+      return false if @board[coord].is_bomb?
+      if @board[coord].is_revealed?
+        puts "Chosen square already revealed. Pick another square."
+        sleep(2)
+      end
+      if @board[coord].is_flagged?
+        puts "Chosen square is flagged. Unflag it or pick another square"
+        sleep(2)
+      else
+        @board.reveal_tile(coord)
+      end
+    elsif command == 'f'
+      @board.toggle_flag(coord)
+    end
+    return true
+  end
+
+  def game_over()
+    system("clear")
+    puts "Game over!".colorize(:red)
+    puts
+    @board.reveal_all_bombs
+    @board.render
+  end
+
+  def you_win()
+    system("clear")
+    puts "You WIN!".colorize(:green)
+    puts
+    @board.render
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  game = Minesweeper.new
+  game.start
+end
---
+++ player.rb
@@ -0,0 +1,36 @@
+class Player
+  def initialize(size)
+    @size = size
+  end
+  
+  def get_coordinate
+    loop do
+      puts "Enter coordinates separated by comma. ex: 'x,y'"
+      coord = gets.chomp
+      if valid_coord(coord)
+        return coord.split(",").map do |c|
+          c.to_i - 1
+        end
+      end
+      puts "Coordinates not valid. Try again."
+    end
+  end
+
+  def get_command
+    loop do
+      puts "Enter 'r' to reveal or 'f' to flag:"
+      command = gets.chomp
+      return command if self.valid_command(command)
+      puts "Command is not valid. Try again."
+    end
+  end
+
+  def valid_coord(coord)
+    return true if /^\s*[1-#{@size}]\s*\,\s*[1-#{@size}]\s*$/.match(coord)
+    return false
+  end
+
+  def valid_command(command)
+    command.downcase == 'r' || command.downcase == 'f'
+  end
+end
